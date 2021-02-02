86158c6be333d305887a55ccf51747211a60e861
root <oukssim2015@gmail.com>
2021-02-02 18:38:14 +0100

    Added Ingredient List endpoints

--- app/recipe/views.py
+++ app/recipe/views.py
@@ -2,7 +2,7 @@ from rest_framework import viewsets, mixins
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 
-from appcore.models import Tag
+from appcore.models import Tag, Ingredient
 
 from . import serializers
 
@@ -10,12 +10,29 @@ from . import serializers
 class TagViewSet(viewsets.GenericViewSet,
                  mixins.ListModelMixin,
                  mixins.CreateModelMixin):
-
     """Manage Tag in the database"""
     authentication_classes = (TokenAuthentication,)
     permission_classes = (IsAuthenticated,)
     queryset = Tag.objects.all()
     serializers_class = serializers.TagSerializer
+
+    def get_queryset(self):
+        """Return objects for current auth user only"""
+        return self.queryset.filter(user=self.request.user).order_by('-name')
+
+    def perform_create(self, serializer):
+        """Create new tag"""
+        serializer.save(user=self.request.user)
+
+
+class IngredientViewSet(viewsets.GenericViewSet,
+                        mixins.ListModelMixin,
+                        mixins.CreateModelMixin):
+    """Manage Ingredient in the database"""
+    authentication_classes = (TokenAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    queryset = Ingredient.objects.all()
+    serializers_class = serializers.IngredientSerializer
 
     def get_queryset(self):
         """Return objects for current auth user only"""
